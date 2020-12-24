e1ae6eb43c1a6901bb49e133c95a7a4f84d43059
musfirahrk <musfirahriaz@gmail.com>
2020-12-24 20:09:56 +0500

    Updated carasol

--- src/app/carasol-component/carasol-component.component.ts
+++ src/app/carasol-component/carasol-component.component.ts
@@ -5,46 +5,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carasol-component',
   templateUrl: './carasol-component.component.html',
-  styleUrls: ['./carasol-component.component.scss'],
+  styleUrls: ['./carasol-component.component.css'],
 })
 export class  CarasolComponentComponent implements OnInit {
   
   cards = [
     {
       title: 'Card Title 1',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
+      Author: 'Button',
+      subject:'sub',
      
     },
     {
       title: 'Card Title 2',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
+      Author: 'Button',
+      subject:'sub',
 
     },
     {
       title: 'Card Title 3',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
+      Author: 'Button',
+      subject:'sub',
       
     },
     {
       title: 'Card Title 4',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
+      Author: 'Button',
+      subject:'sub',
      
     },
     {
       title: 'Card Title 5',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
+      Author: 'Button',
+      subject:'sub',
 
     },
     {
       title: 'Card Title 6',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      
+      Author: 'Button',
+      subject:'sub',
+    },
+    {
+      title: 'Card Title 6',
+      Author: 'Button',
+      subject:'sub',
+    },
+    {
+      title: 'Card Title 6',
+      Author: 'Button',
+      subject:'sub',
     },
   
   ];
@@ -56,8 +65,86 @@ export class  CarasolComponentComponent implements OnInit {
     }
     return R;
   }
+  personHistory = [
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+      
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+     
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+    },
+    {
+      Name : 'Person Name',
+      FineDue : 'None',
+      FinePaid: ' Rs 15',
+      bookName: 'Blah',
+      ret_date: '20/1/20 ',
+
+    },
+  
+  ];
+
+  slideshistory: any = [[]];
+  chunkhistory(arr: string | any[], chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
   ngOnInit() {
-    this.slides = this.chunk(this.cards, 3);
+    this.slides = this.chunk(this.cards, 4);
+    this.slideshistory = this.chunkhistory(this.personHistory, 4);
   }
 }
 
