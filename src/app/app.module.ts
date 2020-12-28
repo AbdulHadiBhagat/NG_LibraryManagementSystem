478bd6ce8045d681d8927b947ef8dc0ec40ce65d
musfirahrk <musfirahriaz@gmail.com>
2020-12-28 20:22:57 +0500

    Sent my components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { DxSelectBoxModule,
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 //import { BookCardsComponent } from './modules/Common/book-cards.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
+import { HomeComponentComponent } from './modules/home-component/home-component.component';
 //import { PersonHistoryCardComponent } from './modules/Common/person-history-card/person-history-card.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BorrowerComponent } from './modules/Borrower/borrower.component';
@@ -26,7 +26,7 @@ import { BookComponent } from './modules/Book/book.component';
 import { LibrarianComponent } from './modules/Librarian/librarian.component';
 import { PersonComponent } from './modules/Person/person.component';
 
-import { CarasolComponentComponent } from './carasol-component/carasol-component.component';
+import { CarasolComponentComponent } from './modules/carasol-component/carasol-component.component';
 
 
 
